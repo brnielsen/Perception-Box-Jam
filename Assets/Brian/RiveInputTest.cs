2a6f3d67139fb51b5de8d3649f685880d541edb2
Brian Nielsen <brian.nielsen88@gmail.com>
2025-03-02 16:02:08 -0500

    player movement testing

--- Assets/Brian/RiveInputTest.cs
+++ Assets/Brian/RiveInputTest.cs
@@ -6,6 +6,14 @@ public class RiveInputTest : MonoBehaviour
 {
     [SerializeField] private RiveWidget _riveWidget;
     StateMachine _stateMachine;
+    SMITrigger _idleTrigger;
+    SMITrigger _runTrigger;
+
+    [Header("Movement References")]
+    [SerializeField] private RectTransform _playerRectTransform;
+    [SerializeField] private float _moveSpeed = 10f;
+
+    bool _isRunning;
 
     private void Start()
     {
@@ -14,10 +22,48 @@ public class RiveInputTest : MonoBehaviour
         foreach (var input in _stateMachine.Inputs())
         {
             Debug.Log(input.Name);
+            if (input.Name == "Run Trigger")
+            {
+                _runTrigger = _stateMachine.GetTrigger(input.Name);
+            }
+
+            if (input.Name == "Idle Trigger")
+            {
+                _idleTrigger = _stateMachine.GetTrigger(input.Name);
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (_isRunning == false)
+            {
+                _isRunning = true;
+                _idleTrigger.Fire();
+            }
+            _playerRectTransform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * _moveSpeed, 0, 0));
+            if(Input.GetAxisRaw("Horizontal") > 0){
+                _playerRectTransform.localScale = new Vector3(-1, 1, 1);
+            }else{
+                _playerRectTransform.localScale = new Vector3(1, 1, 1);
+            }
+
+        }
+        else
+        {
+            if (_isRunning == true)
+            {
+                _isRunning = false;
+                _idleTrigger.Fire();
+            }
+
         }
     }
 
-[ContextMenu("Run Trigger")]
+    [ContextMenu("Run Trigger")]
     public void SetTrigger()
     {
         SMITrigger sMITrigger = _stateMachine.GetTrigger("RunTrigger");
