9731526c4d1cfaadacb8204faf33b3974a61177f
Brian Nielsen <brian.nielsen88@gmail.com>
2025-03-04 19:39:37 -0500

    Breathing game transitions to winnable minigame

--- Assets/Brian/Minigame Testing/BreathingGameControls.cs
+++ Assets/Brian/Minigame Testing/BreathingGameControls.cs
@@ -1,15 +1,48 @@
+using System;
 using Rive;
 using Rive.Components;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BreathingGameControls : MonoBehaviour
 {
     [SerializeField] private RiveWidget _riveWidget;
 
+    [SerializeField] private int _requiredBreaths = 3;
+
+    public string sceneToLoad = "GameScene";
+
+    private int _currentBreaths = 0;
+
     private StateMachine _stateMachine;
 
     private SMITrigger _breatheTrigger;
+
+    void OnEnable()
+    {
+        _riveWidget.OnRiveEventReported += OnRiveEventReported;
+    }
+
+    void OnDisable()
+    {
+        _riveWidget.OnRiveEventReported -= OnRiveEventReported;
+    }
+
+    private void OnRiveEventReported(ReportedEvent @event)
+    {
+        Debug.Log(@event.Name);
+        if (@event.Name == "OnBreatheComplete")
+        {
+            _currentBreaths++;
+            if (_currentBreaths >= _requiredBreaths)
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         _stateMachine = _riveWidget.StateMachine;
